92ca93153d6540eb8579e852936e248e4f8b4611
Naufal Dwi Maulana <naufaldwimaulana@Naufals-MacBook-Pro.local>
2022-03-18 17:19:28 +0700

    documentasi untuk postman

--- app/Http/Controllers/ShoppingController.php
+++ app/Http/Controllers/ShoppingController.php
@@ -121,23 +121,6 @@ class ShoppingController extends Controller
         }
     }
 
-    // public function getAllCourier() {
-    //     return response()->json(['courier' =>  Courier::all()], 200);
-    // }
-
-    // public function singleCourier($origin, $destination) {
-    //     try {
-    //         $data = Courier::where('destination_name', $destination)->where('origin_name', $origin)->first();
-
-    //         return response()->json(['data' => $data], 200);
-
-    //     } catch (\Exception $e) {
-
-    //         return response()->json(['message' => 'Data not found!'], 404);
-    //     }
-
-    // }
-
 }
 
 
